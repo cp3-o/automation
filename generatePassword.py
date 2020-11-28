5e058055bc4b0e0c84000c4eac18ef838cba7cbb
cp3-o <73436246+cp3-o@users.noreply.github.com>
2020-11-27 19:52:54 -0500

    Update generatePassword.py

--- generatePassword.py
+++ generatePassword.py
@@ -2,7 +2,7 @@ import secrets
 import string 
 
 def password_gen(password_length):
-	characters = string.ascii_etters + string.digits
+	characters = string.ascii_letters + string.digits
 	secure_password = ''.join(secrets.choice(characters) for i in range(password_length))
 
 	return secure_password
